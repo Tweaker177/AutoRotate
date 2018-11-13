873e38d7b31fcdaf124d52f8ae51bb1a64dfdeb5
i0s_tweak3r <djvs23@gmail.com>
2018-11-13 18:07:27 -0500

    Add files via upload

--- AutoRotate/AutoRotate.mm
+++ AutoRotate/AutoRotate.mm
@@ -1,16 +1,34 @@
+#import <spawn.h>
+#import <objc/runtime.h>
 #import <Preferences/Preferences.h>
+#import <Preferences/PSListController.h>
+#import <Preferences/PSSpecifier.h>
+#import <Preferences/PSTableCell.h>
 
-@interface AutoRotateListController: PSListController {
-}
+@interface AutoRotateListController: PSListController 
+
+- (void)respring:(id)sender;
+-(void)twitter;
+-(void)donate;
+-(void)twitter2;
+-(void)repolink;
+-(void)link;
 @end
 
 @implementation AutoRotateListController
 - (id)specifiers {
 	if(_specifiers == nil) {
-		_specifiers = [[self loadSpecifiersFromPlistName:@"AutoRotate" target:self] retain];
+		_specifiers = [self loadSpecifiersFromPlistName:@"AutoRotate" target:self];
 	}
 	return _specifiers;
 }
+- (void)respring:(id)sender {
+	pid_t pid;
+    const char* args[] = {"killall", "SpringBoard", NULL};
+    posix_spawn(&pid, "/usr/bin/killall", NULL, NULL, (char* const*)args, NULL);
+}
+
+
 
 - (void)twitter {
     if ([[UIApplication sharedApplication] canOpenURL:[NSURL URLWithString:@"twitter://user?screen_name=brianvs"]]) {
@@ -26,6 +44,10 @@
     [[UIApplication sharedApplication] openURL:[NSURL URLWithString:@"http://twitter.com/prouser"]];
 } 
 
+- (void)donate
+{
+    [[UIApplication sharedApplication] openURL:[NSURL URLWithString:@"https://paypal.me/i0stweak3r"]];
+}
 - (void)repolink {
     [[UIApplication sharedApplication] openURL:[NSURL URLWithString:@"https://i0s-tweak3r-betas.yourepo.com"]];
 } 
